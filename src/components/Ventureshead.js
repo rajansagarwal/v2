75744c4c847fc4192b539b7f0d1fa655cfe05330
Rajan Agarwal <64426829+itsrajan@users.noreply.github.com>
2021-08-28 13:22:50 +0000

    centre ventures page

--- src/components/Ventureshead.js
+++ src/components/Ventureshead.js
@@ -6,8 +6,8 @@ class Ventureshead extends Component {
   render() {
     return (
       <div>
-        <h2 className="explorehead hey">A few non-work endeavours</h2>
-				<div className="explorehead moving"><Subnav /><br /></div>
+        <h2 className="ventureshead hey">A few non-work endeavours</h2>
+				<div className="ventureshead moving"><Subnav /><br /></div>
       </div>
     );
   }
