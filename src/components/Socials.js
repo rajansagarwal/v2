a8c2348a92be9eae7b6d2db5c75bbb62a04cfebb
Rajan Agarwal <64426829+itsrajan@users.noreply.github.com>
2021-10-08 13:46:31 +0000

    fix warnings

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import "./Component.css";
-import { Link } from 'react-router-dom';
 
 class Socials extends Component {
   render() {
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,6 @@ import React from "react";
 import { Container, Row, Col } from "react-grid-system";
 import "../App.css";
 import Transition from "../components/Transition";
-import Subnav from "../components/Subnav";
 import Manifesto from "../components/Manifesto";
 import Highlights from "../components/Highlights";
 import Socials from "../components/Socials";
