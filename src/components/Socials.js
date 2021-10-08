1b5e8d13ca84629f4dd1b55f810f18828e1da0b8
Rajan Agarwal <64426829+itsrajan@users.noreply.github.com>
2021-10-08 15:32:15 +0000

    add accents

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -7,8 +7,7 @@ class Socials extends Component {
       <div>
         <h5 className="manifesto">Pourqoui habitons-nous dans les arbres?</h5>
         <p className="p-manifesto">
-          Albert Einstein a dit un jour, “La plupart des bonnes choses viennent des arbres”. En donnant de l'oxygène, un espace vivant et les merveilles de la nature. Nous voulons faire un envrionnement ou vous pouvez exprimer notre experiences s'en sentant saufé.  <br/><br/>
-					<img src="https://loveincorporated.blob.core.windows.net/contentimages/gallery/79862735-4f21-4ea1-9b7e-1d458a326182-silver-maple-treehouse.jpg" width="100%" alt="houseintree"/>
+          Albert Einstein a dit un jour : « La plupart des bonnes choses viennent des arbres ». En apportant de l'oxygène, un espace de vie et les merveilles de la nature. Nous voulons créer un environnement où vous pouvez exprimer nos expériences en vous sentant en sécurité.	<img src="https://loveincorporated.blob.core.windows.net/contentimages/gallery/79862735-4f21-4ea1-9b7e-1d458a326182-silver-maple-treehouse.jpg" width="100%" alt="houseintree"/>
         </p>
       </div>
     );
